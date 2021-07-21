203e40f94027b2edc4db3db6de1a4ed7f6d79e45
roy <roy.framery@gmail.com>
2021-07-21 14:05:07 +0200

    Added the fonctionnality to close the modal Issues 1/5 : "TODO : fermer la modale" is done

---
+++ modal.js
@@ -0,0 +1,31 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector('.close');
+const modalContent = document.querySelector('.content');
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal event
+modalClose.addEventListener('click', closeModal);
+
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
